612396ed4f3fce36d28b150121c2a8dd189ec593
Vasily Ponomarev <mbizon666@yandex.ru>
2020-09-10 23:07:36 +0300

    Updates

---
+++ precache-manifest.97ae3fd884cf42bf99bdd500e33b9dbc.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09331ef72f04dfbc98d5545aa11db082",
+    "url": "/VK3-2/index.html"
+  },
+  {
+    "revision": "1d35b952a0b94eb9867b",
+    "url": "/VK3-2/static/css/main.2a8fade7.chunk.css"
+  },
+  {
+    "revision": "614ce14be01ddc6b2e0e",
+    "url": "/VK3-2/static/js/2.3b433b07.chunk.js"
+  },
+  {
+    "revision": "41b66b6fd6eecba581340794bb1831be",
+    "url": "/VK3-2/static/js/2.3b433b07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d35b952a0b94eb9867b",
+    "url": "/VK3-2/static/js/main.e9ac743f.chunk.js"
+  },
+  {
+    "revision": "644dfe425ca2b13a7301",
+    "url": "/VK3-2/static/js/runtime-main.4e208aae.js"
+  },
+  {
+    "revision": "dfc54362fc2b0f7b99a4038b87dd9e64",
+    "url": "/VK3-2/static/media/1.dfc54362.png"
+  },
+  {
+    "revision": "2048975925bd8553d8aaa5e1d9d1ffb1",
+    "url": "/VK3-2/static/media/2.20489759.png"
+  },
+  {
+    "revision": "dbf82c38432d45de3c1c99f13c8b5fee",
+    "url": "/VK3-2/static/media/3.dbf82c38.png"
+  },
+  {
+    "revision": "e671d53961f44185ee61256a0e6f13a5",
+    "url": "/VK3-2/static/media/4.e671d539.png"
+  },
+  {
+    "revision": "35bcd06768ffafae4a5721b8f0c4fd8f",
+    "url": "/VK3-2/static/media/burger.35bcd067.png"
+  },
+  {
+    "revision": "dd480b0d953c2e73dd0c54d7617d391a",
+    "url": "/VK3-2/static/media/check-symbol.dd480b0d.svg"
+  },
+  {
+    "revision": "3e408ef227a082feb9d3527437ef78c2",
+    "url": "/VK3-2/static/media/edit.3e408ef2.svg"
+  },
+  {
+    "revision": "4fd2ba47b4f59d0a4382a8d81aae85d6",
+    "url": "/VK3-2/static/media/kfc.4fd2ba47.png"
+  },
+  {
+    "revision": "60464810e8cfcde7a20fc19284c38a0e",
+    "url": "/VK3-2/static/media/mcdac.60464810.png"
+  },
+  {
+    "revision": "36053a5b28bf11d11c66adc05394d4d3",
+    "url": "/VK3-2/static/media/refresh-button.36053a5b.svg"
+  },
+  {
+    "revision": "4eb05fdd9f3d81f2f81cac5847e25cc9",
+    "url": "/VK3-2/static/media/sub.4eb05fdd.png"
+  }
+]);
